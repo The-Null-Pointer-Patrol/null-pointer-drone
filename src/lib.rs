d94db34f9c99cd4e2252a73b27571ebbb5d5bcd9
marcoff181 <filippomarcon66@gmail.com>
2024-11-21 18:23:43 +0100

    init repo with examples and add basic dependencies

---
+++ src/lib.rs
@@ -0,0 +1,79 @@
+use crossbeam_channel::{select, Receiver, Sender};
+use std::collections::HashMap;
+use std::thread;
+use wg_2024::controller::Command;
+use wg_2024::drone::Drone;
+use wg_2024::network::NodeId;
+use wg_2024::packet::{Packet, PacketType};
+
+pub struct MyDrone {
+    id: NodeId,
+    sim_contr_send: Sender<Command>,
+    sim_contr_recv: Receiver<Command>,
+    packet_recv: Receiver<Packet>,
+    pdr: u8,
+    packet_send: HashMap<NodeId, Sender<Packet>>,
+}
+
+impl Drone for MyDrone {
+    fn new(options: wg_2024::drone::DroneOptions) -> Self {
+        Self {
+            id: options.id,
+            sim_contr_send: options.sim_contr_send,
+            sim_contr_recv: options.sim_contr_recv,
+            packet_recv: options.packet_recv,
+            pdr: (options.pdr * 100.0) as u8,
+            packet_send: HashMap::new(),
+        }
+    }
+
+    fn run(&mut self) {
+        loop {
+            select! {
+                recv(self.packet_recv) -> packet_res => {
+                    if let Ok(packet) = packet_res {
+                        match packet.pack_type {
+                            PacketType::Nack(_nack) => unimplemented!(),
+                            PacketType::Ack(_ack) => unimplemented!(),
+                            PacketType::MsgFragment(_fragment) => unimplemented!(),
+                            PacketType::FloodRequest(_) => unimplemented!(),
+                            PacketType::FloodResponse(_) => unimplemented!(),
+                        }
+                    }
+                },
+                recv(self.sim_contr_recv) -> command_res => {
+                    if let Ok(command) = command_res {
+                        match command{
+                            Command::AddChannel(_, sender) => todo!(),
+                            Command::RemoveChannel(_) => todo!(),
+                            Command::Crash => todo!(),
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl MyDrone {
+    fn add_channel(&mut self, id: NodeId, sender: Sender<Packet>) {
+        self.packet_send.insert(id, sender);
+    }
+
+    // fn remove_channel(...) {...}
+}
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
