e5d90e6f4cc668c0ce674dee65d69bd3d4310258
di-bon <dibon.francesco03@gmail.com>
2024-11-22 20:28:04 +0100

    Start implementing packet forwarding

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,19 @@
 use crossbeam_channel::{select, Receiver, Sender};
 use std::collections::HashMap;
-use std::thread;
+use std::time::{Instant};
+use rand::Rng;
 use wg_2024::controller::Command;
 use wg_2024::drone::Drone;
-use wg_2024::network::NodeId;
-use wg_2024::packet::{Packet, PacketType};
+use wg_2024::network::{NodeId, SourceRoutingHeader};
+use wg_2024::packet::{Nack, NackType, Packet, PacketType};
 
+// TODO: add missing field 'known_flood_ids: HashSet<u64>' to keep track of previous flood requests
 pub struct MyDrone {
     id: NodeId,
     sim_contr_send: Sender<Command>,
     sim_contr_recv: Receiver<Command>,
     packet_recv: Receiver<Packet>,
-    pdr: u8,
+    pdr: f32,
     packet_send: HashMap<NodeId, Sender<Packet>>,
 }
 
@@ -22,7 +24,7 @@ impl Drone for MyDrone {
             sim_contr_send: options.sim_contr_send,
             sim_contr_recv: options.sim_contr_recv,
             packet_recv: options.packet_recv,
-            pdr: (options.pdr * 100.0) as u8,
+            pdr: options.pdr * 100.0,
             packet_send: HashMap::new(),
         }
     }
@@ -32,10 +34,44 @@ impl Drone for MyDrone {
             select! {
                 recv(self.packet_recv) -> packet_res => {
                     if let Ok(packet) = packet_res {
-                        match packet.pack_type {
-                            PacketType::Nack(_nack) => unimplemented!(),
-                            PacketType::Ack(_ack) => unimplemented!(),
-                            PacketType::MsgFragment(_fragment) => unimplemented!(),
+                        match &packet.pack_type {
+                            PacketType::Nack(nack) => {
+                                let fragment_index = nack.fragment_index;
+                                match self.forward_packet(packet.clone()) {
+                                    Ok(()) => {},
+                                    Err(error) => {
+                                        let error_packet = create_error_packet(&packet, error, fragment_index);
+                                        let _ = self.forward_packet(error_packet);
+                                    }
+                                }
+                            },
+                            PacketType::Ack(ack) => {
+                                let fragment_index = ack.fragment_index;
+                                match self.forward_packet(packet.clone()) {
+                                    Ok(()) => {},
+                                    Err(error) => {
+                                        let error_packet = create_error_packet(&packet, error, fragment_index);
+                                        let _ = self.forward_packet(error_packet);
+                                    }
+                                }
+                            },
+                            PacketType::MsgFragment(fragment) => {
+                                let fragment_index = fragment.fragment_index;
+                                let mut rng = rand::thread_rng();
+                                let random_number: f32 = rng.gen_range(0.0..=1.0);
+                                if random_number <= self.pdr {
+                                    let error_packet = create_error_packet(&packet, NackType::Dropped, fragment_index);
+                                    let _ = self.forward_packet(error_packet);
+                                } else {
+                                    match self.forward_packet(packet.clone()) {
+                                        Ok(()) => {},
+                                        Err(error) => {
+                                            let error_packet = create_error_packet(&packet, error, fragment_index);
+                                            let _ = self.forward_packet(error_packet);
+                                        }
+                                    }
+                                }
+                            },
                             PacketType::FloodRequest(_) => unimplemented!(),
                             PacketType::FloodResponse(_) => unimplemented!(),
                         }
@@ -55,16 +91,72 @@ impl Drone for MyDrone {
     }
 }
 
+fn create_error_packet(original_packet: &Packet, nack_type: NackType, fragment_index: u64) -> Packet {
+    let hop_index = original_packet.routing_header.hop_index;
+    let reverse_path = original_packet
+        .routing_header
+        .hops
+        .split_at(hop_index + 1)
+        .0
+        .iter()
+        .rev()
+        .copied()
+        // .map(|node| *node) // copied substitutes this map call
+        .collect::<Vec<NodeId>>();
+
+    let mut rng = rand::thread_rng();
+    let new_session_id: u64 = rng.random();
+    Packet {
+        pack_type: PacketType::Nack(
+            Nack {
+                fragment_index,
+                time_of_fail: Instant::now(),
+                nack_type,
+            }
+        ),
+        routing_header: SourceRoutingHeader {
+            hop_index: 0,
+            hops: reverse_path
+        },
+        session_id: new_session_id, // TODO: ask how session_id's are generated
+    }
+}
+
 impl MyDrone {
+    // TODO: ask what to do if a nack/ack packet contains invalid routing
+    // Note: by protocol constraint, a drone cannot check hop_index field.
+    // Therefore, you have to rely on other types of checks to verify whether a drone
+    // is the destination of a packet and, if so, signal the logical error
+    fn forward_packet(&self, mut packet: Packet) -> Result<(), NackType> {
+        packet.routing_header.hop_index += 1;
+        let hop_index = packet.routing_header.hop_index;
+        let next_hop = packet.routing_header.hops.get(hop_index);
+        match next_hop {
+            Some(next_node) => {
+                match self.packet_send.get(next_node) {
+                    Some(next_node_channel) => {
+                        let _ = next_node_channel.send(packet);
+                        Ok(())
+                    },
+                    None => {
+                        Err(NackType::ErrorInRouting(*next_node))
+                    }
+                }
+            },
+            None => {
+                // if the match expression returns None, it means that the current drone
+                // is the designed destination
+                Err(NackType::DestinationIsDrone)
+            }
+        }
+    }
+
+
     fn add_channel(&mut self, id: NodeId, sender: Sender<Packet>) {
         self.packet_send.insert(id, sender);
     }
 
     // fn remove_channel(...) {...}
-}
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
 }
 
 // #[cfg(test)]
